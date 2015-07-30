09b0510b1b36ce42cceab6aa22ab74948f863005
Stephen Cannon <stephentcannon@gmail.com>
2015-07-30 17:53:01 -0400

    wtf

---
+++ test/client/frob.js
@@ -0,0 +1,9 @@
+// Template.registerHelper('inputField', function(option){
+//   //options.fn(this)
+//   console.log('option.hash.label: ' + option.hash.label);
+//   var html = '<input type="' + option.hash.type + '">';
+
+//   return Spacebars.SafeString(html);
+// });
+
+//frob(a, b, c, Spacebars.kw({verily: true}))
